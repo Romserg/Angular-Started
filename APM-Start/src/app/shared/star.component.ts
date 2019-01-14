e0f4ac4fa2b67b27bffc10d1048fd78e32e228a9
Romserg <serg.romankov@gmail.com>
2019-01-14 21:15:27 +0200

    Implement star rating displaying with page title

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -9,8 +9,14 @@ import { Component, Input, OnChanges } from '@angular/core';
 export class StarComponent implements OnChanges {
   @Input() rating;
   starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter();
 
   ngOnChanges(): void {
     this.starWidth = this.rating * 75 / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 }
